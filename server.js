d481d046de3f00b490baaac68c158304957abb61
jovana <jovanatasic39@gmail.com>
2024-06-28 00:42:32 +0200

    added new route for user

--- server.js
+++ server.js
@@ -9,21 +9,25 @@ app.get('/', function (req, res) {
    return res.send('Hello Jovana');
 })
 
-app.get('/users', (req, res) => {
-    res.send('Ovde su korisnici');
-});
-  
 
-app.get('/user', (req, res) => {
-    res.send('Samo jedan user');
-});
-   
-app.get('/email', (req, res) => {
-    res.send('Unesite vas email');
-});
 app.get('/report', (req, res) => {
     res.send('Getting report list');
 });
+
+app.get('/user', (req, res) => {
+    res.send('Add user');
+});
+
+app.post('/user', (req, res) => {
+    res.send('Create user');
+});
+app.patch('/user', (req, res) => {
+    res.send('Update user');
+});
+app.delete('/user', (req, res) => {
+    res.send('Delete user');
+});
+
 
 
   
