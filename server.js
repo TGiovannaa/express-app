3da101d59f6f272e984b940519247651fa8f51a6
jovana <jovanatasic39@gmail.com>
2024-07-04 21:15:55 +0200

    added email in post and patch routes

--- server.js
+++ server.js
@@ -54,8 +54,8 @@ app.get('/user/:id', (req, res) => {
 app.post('/user', (req, res) => {
     const newUser = {
         id: users.length + 1, 
-        name: req.body.name
-       
+        name: req.body.name,
+        email: req.body.email
     };
 
     
@@ -65,12 +65,13 @@ app.post('/user', (req, res) => {
     res.status(200).send(newUser);
 });
 
-app.patch('/user/:id', (req, res) => {
+app.patch('/user/:id/email', (req, res) => {
     const userId = parseInt(req.params.id, 10);
     const user = users.find(user => user.id === userId);
 
     if (user) {
         user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
         res.send(user);
     } else {
         res.status(404).send('User not found');
